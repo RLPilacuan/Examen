7f2571130ea338e6095c05afb3de95b8f469af0c
RLPilacuan <roger.pilacuan@hotmail.com>
2018-01-15 11:45:20 -0500

    Ejercicio16

---
+++ Examen2/Ejercicio16/src/ec/edu/espe/conjunta/A.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.conjunta;
+
+/**
+ *
+ * @author Labs-DECC
+ */
+public class A {
+   private B b1;
+   private B b2;
+   public void ml(){
+       
+   }
+}
